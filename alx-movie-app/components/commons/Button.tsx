de8f7c6bc34ececcf72a9c88cf308081a679d9d1
elorm gershon <alidugershon@gmail.com>
2025-08-07 00:51:45 +0000

    Project 2 begun second update

---
+++ alx-movie-app/components/commons/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = () => {
+  return (
+    <button>
+      Common Button
+    </button>
+  );
+};
+
+export default Button;
---
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MovieCard = () => {
+  return (
+    <div>
+      <h3>Movie Title</h3>
+      <p>Movie details here...</p>
+    </div>
+  );
+};
+
+export default MovieCard;
